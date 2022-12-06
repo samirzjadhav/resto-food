1e473b54b0897ba94360459f9add66ad598c42a8
Samir Jadhav <samirzjadhav@gmail.com>
2022-12-06 14:49:43 +0530

    feat: added onclick event on close button

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const menu = document.getElementById("menu-bars");
 const nav = document.getElementById("nav");
+const searchIcon = document.getElementById("search-icons");
+const searchForm = document.getElementById("search-form");
+const close = document.getElementById("close");
 
 menu.onclick = () => {
   menu.classList.toggle("fa-times");
   nav.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  nav.classList.remove("active");
+};
+
+searchIcon.onclick = () => {
+  searchForm.classList.toggle("active");
+};
+
+close.onclick = () => {
+  searchForm.classList.remove("active");
 };
