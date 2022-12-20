fae2ede3fb321486915dc6a40d10b2c2a9d6782d
Samir Jadhav <samirzjadhav@gmail.com>
2022-12-20 19:45:08 +0530

    feat: added script on review-slider manipulate slides

--- script.js
+++ script.js
@@ -35,3 +35,27 @@ const swiper = new Swiper(".home-slider", {
   },
   loop: true,
 });
+
+const swiper1 = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
